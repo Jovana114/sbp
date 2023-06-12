adc5ad448d2f72cf4d1045be2267c180dae021ac
AndjelijaStancic <andjica.stancic@gmail.com>
2023-06-13 00:47:15 +0200

    update

--- queries/queriesNotOptimised.js
+++ queries/queriesNotOptimised.js
@@ -307,3 +307,306 @@ db.feedback.aggregate([
     }
   }
 ]);
+
+//Za svaki proizvod koji ima recenziju prikazati prosečnu ocenu, minimalnu ocenu i maksimalnu ocenu koju je dobio. Takođe prikazati koliko pozitivnih i negativnih recenizija ima i koliko je puta bio preporučen
+
+db.feedback.aggregate([
+  {
+    $lookup: {
+      from: "product_info",
+      localField: "product_id",
+      foreignField: "_id",
+      as: "product_info"
+    }
+  },
+  {
+    $unwind: "$product_info"
+  },
+  {
+    $group: {
+      _id: "$product_id",
+      average_rating: { $avg: "$rating" },
+      min_rating: { $min: "$rating" },
+      max_rating: { $max: "$rating" },
+      product_name: { $first: "$product_info.product_name" },
+      brand_name: { $first: "$product_info.brand_name" },
+      recommendation_count: {
+        $sum: {
+          $cond: [
+            { $ne: ["$is_recommended", ""] },
+            { $toDouble: "$is_recommended" },
+            0
+          ]
+        }
+      },
+      total_neg_feedback_count: { $sum: { $ifNull: ["$total_neg_feedback_count", 0] } },
+      total_pos_feedback_count: { $sum: { $ifNull: ["$total_pos_feedback_count", 0] } }
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      average_rating: { $round: ["$average_rating", 2] },
+      min_rating: 1,
+      max_rating: 1,
+      product_name: 1,
+      brand_name: 1,
+      recommendation_count: 1,
+      total_neg_feedback_count: 1,
+      total_pos_feedback_count: 1
+    }
+  },
+  {
+    $sort: {
+      product_name: -1
+    }
+  }
+]);
+
+
+//Za svaki brend čiji proizvodi imaju recenzije, izračunati koliko preporuka ukupno imaju njegovi proizvodi, i ukupan broj pozitivnih i negativnih recenzija za sve njegove proizvode
+
+
+db.feedback.aggregate([
+  {
+    $lookup: {
+      from: "product_info",
+      localField: "product_id",
+      foreignField: "_id",
+      as: "product_info"
+    }
+  },
+  {
+    $unwind: "$product_info"
+  },
+  {
+    $group: {
+      _id: "$product_info.brand_name",
+      recommendation_count: {
+        $sum: {
+          $cond: [
+            { $ne: ["$is_recommended", ""] },
+            { $toDouble: "$is_recommended" },
+            0
+          ]
+        }
+      },
+      total_neg_feedback_count: { $sum: "$total_neg_feedback_count" },
+      total_pos_feedback_count: { $sum: "$total_pos_feedback_count" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      brand_name: "$_id",
+      recommendation_count: 1,
+      total_neg_feedback_count: 1,
+      total_pos_feedback_count: 1
+    }
+  },
+  {
+    $sort: {
+      recommendation_count: -1
+    }
+  }
+]);
+
+//Za svaki brend koji ima recenzije svojih proizvoda pronaći profil osobe koja najčešće predlaže njihove proizvode
+
+db.feedback.aggregate([
+  {
+    $lookup: {
+      from: "product_info",
+      localField: "product_id",
+      foreignField: "_id",
+      as: "product_info"
+    }
+  },
+  {
+    $unwind: "$product_info"
+  },
+  {
+    $match: {
+      is_recommended: "1.0"
+    }
+  },
+  {
+    $group: {
+      _id: {
+        brand_name: "$product_info.brand_name",
+        skin_tone: "$skin_tone",
+        skin_type: "$skin_type",
+        hair_color: "$hair_color"
+      },
+      positive_reviews: { $sum: 1 }
+    }
+  },
+  {
+    $sort: {
+      "_id.brand_name": 1,
+      positive_reviews: -1
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.brand_name",
+      profiles: {
+        $push: {
+          skin_tone: "$_id.skin_tone",
+          skin_type: "$_id.skin_type",
+          hair_color: "$_id.hair_color",
+          positive_reviews: "$positive_reviews"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      brand_name: "$_id",
+      profiles: {
+        $slice: ["$profiles", 0, 1]
+      }
+    }
+  },
+  {
+    $sort: {
+      brand_name: 1
+    }
+  }
+]);
+
+//Pronaći najbolje ocenjen proizvod za svaki brend na osnovu prosečne ocene i ukupnog broja preporuka korisnika u recenzijama
+
+db.feedback.aggregate([
+  {
+    $lookup: {
+      from: "product_info",
+      localField: "product_id",
+      foreignField: "_id",
+      as: "product_info"
+    }
+  },
+  {
+    $unwind: "$product_info"
+  },
+  {
+    $match: {
+      is_recommended: "1.0",
+      "product_info.brand_name": { $ne: "" }
+    }
+  },
+  {
+    $group: {
+      _id: {
+        brandName: "$product_info.brand_name",
+        productId: "$product_info._id",
+        productName: "$product_info.product_name",
+        primaryCategory: "$product_info.primary_category",
+        secondaryCategory: "$product_info.secondary_category",
+        tertiaryCategory: "$product_info.tertiary_category"
+      },
+      avgRating: {
+        $avg: { $toDouble: "$rating" }  // Calculate the average rating using $avg and $toDouble
+      },
+      totalRecommendations: { $sum: 1 }  // Count the number of recommendations
+    }
+  },
+  {
+    $sort: {
+      "_id.brandName": 1,
+      avgRating: -1,  // Sort by average rating in descending order
+      totalRecommendations: -1  // Sort by total recommendations in descending order
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.brandName",
+      topProduct: {
+        $first: {
+          productId: "$_id.productId",
+          productName: "$_id.productName",
+          primaryCategory: "$_id.primaryCategory",
+          secondaryCategory: "$_id.secondaryCategory",
+          tertiaryCategory: "$_id.tertiaryCategory",
+          avgRating: { $divide: [ { $round: ["$avgRating", 1] }, 1 ] },  // Round average rating to 1 decimal place
+          totalRecommendations: "$totalRecommendations"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      brandName: "$_id",
+      topProduct: 1
+    }
+  },
+  {
+    $sort: {
+      brandName: 1
+    }
+  }
+]);
+
+//Za svaki tip kože korisnika u bazi (dry, combination, sensetive, normal) pronaći po 2 proizvoda koja spadaju u "Moisturizers" kategoriju (tertiary_category) a koja su ljudi sa tim tipom kože najviše preporučivali u recenzijama 
+
+db.feedback.aggregate([
+  {
+    $lookup: {
+      from: "product_info",
+      localField: "product_id",
+      foreignField: "_id",
+      as: "product_info"
+    }
+  },
+  {
+    $unwind: "$product_info"
+  },
+  {
+    $match: {
+      "product_info.secondary_category": "Moisturizers",
+      is_recommended: "1.0"
+    }
+  },
+  {
+    $group: {
+      _id: {
+        skin_type: "$skin_type",
+        product_id: "$product_id",
+        product_name: "$product_info.product_name",
+        brand_name: "$product_info.brand_name"
+      },
+      count: {
+        $sum: 1
+      }
+    }
+  },
+  {
+    $sort: {
+      "_id.skin_type": 1,
+      count: -1
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.skin_type",
+      products: {
+        $push: {
+          product_id: "$_id.product_id",
+          product_name: "$_id.product_name",
+          brand_name: "$_id.brand_name",
+          count: "$count"
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      products: {
+        $slice: ["$products", 0, 2]
+      }
+    }
+  }
+]);
