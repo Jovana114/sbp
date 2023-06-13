50742b5945dc437113d3f585833d7e0e830d0222
unknown <81173605+Jovana114@users.noreply.github.com>
2023-06-13 09:35:13 +0200

    index fix

--- queries/indexes.js
+++ queries/indexes.js
@@ -1,10 +1,4 @@
-db.product_info.createIndex({ "feedback.is_recommended": 1 });
-db.product_info.createIndex({ "brand_name": 1 });
-db.product_info.createIndex({ "_id": 1 });
-db.product_info.createIndex({ "feedback.rating": 1 });
-db.product_info.createIndex({
-  "feedback.hair_color": 1,
-  "feedback.skin_type": 1,
-  "feedback.eye_color": 1
-});
-db.product_info.createIndex({ "tertiary_category": 1 });
+db.product_with_feedbacks.createIndex({ "brand_name": 1 });
+db.product_with_feedbacks.createIndex({ "_id": 1 });
+db.product_with_feedbacks.createIndex({ "feedback.rating": 1 });
+db.product_with_feedbacks.createIndex({ "tertiary_category": 1 });
